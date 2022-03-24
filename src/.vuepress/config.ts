c1ac99c7d64534ba19d5f2335772e6784e61a497
No F Way <80670048+nfway@users.noreply.github.com>
2022-03-24 12:00:47 +0800

    Add files via upload

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,72 @@
+import { addViteOptimizeDepsInclude } from "@mr-hope/vuepress-shared";
+import { path } from "@vuepress/utils";
+import { defineHopeConfig } from "vuepress-theme-hope";
+import themeConfig from "./themeConfig";
+
+const base = (process.env.BASE as "/" | `/${string}/`) || "/";
+
+export default defineHopeConfig({
+  base,
+
+  dest: "./dist",
+
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "google-site-verification",
+        content: "qG3soux9jAKB4Q_DYf7yj1p5cEIuib6yG4zDhpmv2_E",
+      },
+    ],
+  ],
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "vuepress-theme-hope",
+      description: "A vuepress theme with tons of features✨",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "vuepress-theme-hope",
+      description: "一个具有强大功能的 vuepress 主题✨",
+    },
+  },
+
+  themeConfig,
+
+  alias: {
+    "@IconDisplay": path.resolve(__dirname, "./components/IconDisplay"),
+    "@KatexPlayground": path.resolve(__dirname, "./components/KatexPlayground"),
+    "@theme-hope/components/HomeHero": path.resolve(
+      __dirname,
+      "./components/HopeHero"
+    ),
+  },
+
+  onInitialized: (app) => {
+    if (app.env.isDev)
+      addViteOptimizeDepsInclude(app, [
+        "@mr-hope/vuepress-shared/lib/client",
+        "dayjs",
+        "dayjs/plugin/localizedFormat",
+        "dayjs/plugin/objectSupport",
+        "dayjs/plugin/timezone",
+        "dayjs/plugin/utc",
+      ]);
+
+    addViteOptimizeDepsInclude(app, [
+      "axios",
+      "three",
+      "three/examples/jsm/controls/OrbitControls",
+      "three/examples/jsm/loaders/STLLoader",
+    ]);
+  },
+});
