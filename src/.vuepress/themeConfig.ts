c8c49d5d6c04ad58b3952dc2fc0c857073af2b48
No F Way <80670048+nfway@users.noreply.github.com>
2022-04-14 12:06:23 +0800

    Update themeConfig.ts

--- src/.vuepress/themeConfig.ts
+++ src/.vuepress/themeConfig.ts
@@ -25,92 +25,12 @@ export default defineThemeConfig({
     prefix: "/cookbook/",
     children: ["tutorial", "markdown/", "vuepress/", "style"],
   },
-{
+  {
     text: "迁移",
     link: "/migration/",
   },
-  {
-    text: "下拉菜单",
-    icon: "info",
-    prefix: "/",
-    children: [
-      "changelog",
-      "demo/",
-      "contribution",
-      {
-        text: "插件",
-        icon: "plugin",
-        children: [
-          {
-            text: "AddThis 插件",
-            icon: "share",
-            link: "https://vuepress-theme-hope.github.io/v2/add-this/",
-          },
-          {
-            text: "博客插件",
-            icon: "blog",
-            link: "https://vuepress-theme-hope.github.io/v2/blog/",
-          },
-          {
-            text: "评论插件",
-            icon: "comment",
-            link: "https://vuepress-theme-hope.github.io/v2/comment/",
-          },
-          {
-            text: "组件库",
-            icon: "plugin",
-            link: "https://vuepress-theme-hope.github.io/v2/components/",
-          },
-          {
-            text: "代码复制插件",
-            icon: "copy",
-            link: "https://vuepress-theme-hope.github.io/v2/copy-code/",
-          },
-          {
-            text: "Feed 插件",
-            icon: "rss",
-            link: "https://vuepress-theme-hope.github.io/v2/feed/",
-          },
-          {
-            text: "LightGallery 插件",
-            icon: "pic",
-            link: "https://vuepress-theme-hope.github.io/v2/lightgallery/",
-          },
-          {
-            text: "Markdown 增强插件",
-            icon: "markdown",
-            link: "https://vuepress-theme-hope.github.io/v2/md-enhance/",
-          },
-          {
-            text: "图片预览插件",
-            icon: "pic",
-            link: "https://vuepress-theme-hope.github.io/v2/photo-swipe/",
-          },
-          {
-            text: "PWA 插件",
-            icon: "app",
-            link: "https://vuepress-theme-hope.github.io/v2/pwa/",
-          },
-          {
-            text: "阅读时间插件",
-            icon: "read",
-            link: "https://vuepress-theme-hope.github.io/v2/reading-time/",
-          },
-          {
-            text: "Sass 调色板插件",
-            icon: "palette",
-            link: "https://vuepress-theme-hope.github.io/v2/sass-palette/",
-          },
-          {
-            text: "Seo 插件",
-            icon: "strong",
-            link: "https://vuepress-theme-hope.github.io/v2/seo/",
-          },
-        ],
-      },
-    ],
-  },
-	],
+  
+ ],
 
  
   
